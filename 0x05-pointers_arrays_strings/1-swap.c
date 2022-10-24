1a1d1c207aee400ed744efa739021bc053263481
pablo21gold <pablogold669@gmail.com>
2022-10-24 07:28:07 -0700

    added swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - swap values of two numbs
+ * @b : first pointer
+ * @a : second pointer
+ * Return: vold or nothing
+ */
+
+void swap_int(int *a, int *b);
+{
+int constant =*a;
+*b = *a;
+*a = constant;
+}
+
