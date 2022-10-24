3af36c93ccbf4f55862d7868846a46a1b3631a93
pablo21gold <pablogold669@gmail.com>
2022-10-24 05:53:17 -0700

    created a func that returns vold

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <studio.h>
+
+/**
+ * reset_to_98 - take a pointer and update it to 98
+ * @n : a pointer 
+ * Return : void Basically nothing ls returned
+ */
+
+void reset_to_98(int *n);
+{
+	*n = 98:
+}
+
