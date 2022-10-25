922428082d0cbe313734e6439b834d39b1108297
pablo21gold <pablogold669@gmail.com>
2022-10-25 15:56:19 -0700

    task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -10,12 +10,13 @@ void _puts(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0'p)
+	while (str[i] != '\0')
 	{
-		_putchar(str[i]);
-		i++;
+	_putchar(str[i]);
+	i++;
 	}
-	_putchar('\n')
+
+	_putchar('\n');
 }
 
 
