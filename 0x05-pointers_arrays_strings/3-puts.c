d91370617a763d979b161553c0d9778cb4596841
pablo21gold <pablogold669@gmail.com>
2022-10-25 06:30:29 -0700

    adekunle

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,14 +1,16 @@
 #include "main.h"
 
 /**
- * _puts - prints a string
- * @str: string to be printed
+ * _puts - write string followed by new line
+ * @str: string
+ * Return: void
  */
 void _puts(char *str)
 {
-	while (*str != '\0')
-	{
+	while (*str)
+
 		_putchar(*str++)
-	}
-	_putchar('\n');
+
+
+			_putchar('\n');
 }
