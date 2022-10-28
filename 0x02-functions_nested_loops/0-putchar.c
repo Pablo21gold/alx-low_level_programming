f59d90d9e0a2eeeef0c69d26ccad9f14cab6ee5d
pablo21gold <pablogold669@gmail.com>
2022-10-28 04:23:51 -0700

    adekunle

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int mai(void)
+{
+	char *sh = "main";
+
+	while (*sh)
+	{
+		_putchar('\n');
+
+		return (0);
+	}
